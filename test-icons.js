448cd0611c5f62c767e3689672612ef2ec3d46bc
emrhnozdmr <105025013+emrhnozdmr@users.noreply.github.com>
2025-08-12 17:01:59 +0300

    Delete test-icons.js

--- test-icons.js
+++
@@ -1,3 +0,0 @@
-// This is a small script to log all available icons in lucide-react
-import * as lucide from 'lucide-react';
-console.log(Object.keys(lucide).filter(key => key.toLowerCase().includes('shield')));
